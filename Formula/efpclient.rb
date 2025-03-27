6eb01c9e7b7cb6b63945b43b54d0f2e0492045a1
Antonio Arena <antonio.arena@kaust.edu.sa>
2025-03-27 16:14:13 +0300

    Make sure we install all dependencies.

--- Formula/efpclient.rb
+++ Formula/efpclient.rb
@@ -6,11 +6,14 @@ class Efpclient < Formula
   url "https://test-files.pythonhosted.org/packages/89/0b/c2b82663f0a5e12eb5621b7f020a471d30e53185e2d97adef60ad3a2799d/efpclient-2025.0.2.tar.gz"
   version "25.0.2"
   sha256 "45e2765f55783674b59fe1eca349db655fe19cfdde96c58a6799b01907c1c48c"
+  license "Apache-2.0"
 
   depends_on "python@3.13"
 
   def install
-    virtualenv_install_with_resources
+    venv = virtualenv_create(libexec, python3)
+    venv.pip_install resources
+    venv.pip_install_and_link(buildpath, build_isolation: false)
   end
 
   test do
